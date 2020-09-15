212264bceca5799b49e81ff0962f0a73a5c5ae28
chenda <chend@joywise.net>
2020-09-15 16:55:19 +0800

    首次提交

---
+++ src/components/Video.vue
@@ -0,0 +1,35 @@
+<template>
+    <div ref="videoId"></div>
+</template>
+
+<script>
+    import HlsPlayer from 'xgplayer-hls';
+
+    export default {
+        name: 'HelloWorld',
+        created() {
+        },
+        mounted() {
+            this.initVideo();
+
+        },
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {
+            //官网地址
+            //http://h5player.bytedance.com/gettingStarted/
+            initVideo() {
+                let player = new HlsPlayer({
+                    el: this.$refs.videoId,
+                    url: 'http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8'
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
