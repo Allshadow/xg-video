e1da5ed5aa7af28c32c308987ee971cbb2806328
chenda <chend@joywise.net>
2020-09-15 17:18:05 +0800

    配置自动打开浏览器

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.export = {
     lintOnSave: false, //配置eslint，不生效
     devServer: {
+        open: true,
         overlay: {
             warning: false, //配置eslint，不生效
             errors: false //配置eslint，不生效
